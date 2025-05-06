f3798e3b7e667f4f70d52a5092d5796071efe7c1
muzaffar <smuzaffarov.1996@gmail.com>
2025-05-06 14:31:08 +0900

    generating data in JS

--- costcalculator/costcalculator.js
+++ costcalculator/costcalculator.js
@@ -1,6 +1,42 @@
-let cost = 0;
-let costElement = document.querySelector('.cost-indicator');
-
-function costAdder(){
-  costElement.innerHTML = `$ ${cost}`
-};
+let products = [{
+  name : 'Tea',
+  cost: 2
+}, {
+  name : 'Coffee',
+  cost: 4
+}, {
+  name : 'Coke',
+  cost: 1
+}, {
+  name : 'Fanta',
+  cost: 1
+}, {
+  name : 'Water',
+  cost: 1
+}, {
+  name : 'Plov',
+  cost: 8
+}, {
+  name : 'Shashlik',
+  cost: 6
+}, {
+  name : 'Pizza',
+  cost: 11
+}, {
+  name : 'Burger',
+  cost: 6
+}, {
+  name : 'Pasta',
+  cost: 10
+}
+];
+let allElements = '';
+products.forEach(product=> {
+  allElements += `
+        <div class="item">
+          <p class="name">${product.name}</p>
+          <p class="product-cost">$${product.cost}</p>
+          <button>Order</button>
+        </div>`
+})
+document.querySelector('.food').innerHTML = allElements;
