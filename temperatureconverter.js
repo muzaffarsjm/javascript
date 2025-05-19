04e8c91ed17d7bad9902d6ec6db5e002160bb6b8
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-19 16:26:04 +0900

    in order to improve functions deleted previous functions

--- temperatureconverter.js
+++ temperatureconverter.js
@@ -1,14 +1,2 @@
-function toCelcius(input){
-  let result ;
-  result = Number((input-32)*5/9);
-  console.log(result)
-  let displayElement = document.querySelector('.result-displayer');
-  displayElement.innerHTML = result;
-};
 
-function toFaranheit(input){
-  let result;
-  result=Number((input*9/5)+32);
-  let displayElement = document.querySelector('.result-displayer');
-  displayElement.innerHTML = result;
-};
+
