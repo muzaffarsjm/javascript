29d26ae5f32bf8bd41d90a563a9b4c88ef7cceef
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-19 16:15:16 +0900

    first function created

--- temperatureconverter.js
+++ temperatureconverter.js
@@ -1,1 +1,11 @@
-console.log(56);
+function toCelcius(input){
+  let result;
+  result = (input-32)*5/9;
+  let displayElement = document.querySelector('.result-displayer');
+  displayElement.innerHTML = result;
+};
+
+function toFaranheit(input){
+  let result;
+  result (input*9/5)+32;
+};
