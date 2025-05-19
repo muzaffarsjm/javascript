b0178332c97d6dc2bfe297cc988107aa3e02f502
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-19 16:49:58 +0900

    functions are fully functional

--- temperatureconverter.js
+++ temperatureconverter.js
@@ -1,2 +1,20 @@
+let displayElement = document.querySelector('.result-displayer');
+
+function toCelcius(){
+  let userInput = document.querySelector('.temperature-input');
+  let inputValue = userInput.value;
+  let result = (inputValue - 32)*5/9;
+  displayElement.innerHTML = Math.round(result);
+}
+
+function toFaranheit(){
+  let userInput = document.querySelector('.temperature-input');
+  let inputValue = userInput.value;
+  let result = (inputValue *9/5)+32;
+  displayElement.innerHTML = Math.round(result);
+}
+
+
+
 
 
