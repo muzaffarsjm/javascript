3445bcbcfca3e52cedfbd0df4c02e715804ea102
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-21 23:30:07 +0900

    function description in the comments

--- shoppingcart.js
+++ shoppingcart.js
@@ -1,6 +1,6 @@
 let cartQuantity = 0;
 let quantity = document.querySelector('.quantity-displayer');
-
+//function reders the quantity of products
 function quantityRender(added){
   quantity.innerHTML = `Quantity of products:
   ${cartQuantity+=added}`;
