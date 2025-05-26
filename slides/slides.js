a2f30e895d25d33a1f12b53e62e80977ba8dec09
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-27 03:23:30 +0900

    objects in arrays are created

--- slides/slides.js
+++ slides/slides.js
@@ -1,1 +1,13 @@
-console.log(55);
+const quotesArray = [
+  {
+    name: 'Abdulla Avloniy',
+    photo: 'authorpics/avloniy.png',
+    text: 'Tarbiya biz uchun yo hayot-yo mamot, yo najot-yoo halokat, yo saodat-yo falokat masalasidir'
+  },
+  {
+    name: 'Hamza Hakimzoda Niyoziy',
+    photo: 'authorpics/hamza.png',
+    text: 'Olamning xurshiidi kundir, odamning xurshidi ilm, Olamnning zulmoti tundir, odamning nodori ilm.'
+  }
+  
+];
