a49e5c6957d1ff1c9c363f4a06ed59d77c24b40b
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-26 16:45:02 +0900

    closing button activation checked

--- blog.js
+++ blog.js
@@ -10,7 +10,7 @@ profileIcon.addEventListener('click', ()=>{
   //login page creator html
   let loginPage = `
     <div class = 'styles-login-page'>
-      <button class = 'styles-closing-button'>
+      <button class = 'styles-closing-button js-login-closing-button'>
         X
       </button>
       <button class = 'login-button'>Login</button>
@@ -18,4 +18,10 @@ profileIcon.addEventListener('click', ()=>{
     </div>
   `;
   document.querySelector('.login-page-displayer').innerHTML = loginPage;
+
+  let loginCloser = document.querySelector('.js-login-closing-button');
+loginCloser.addEventListener('click', ()=>{
+  alert('clciked');
 });
+});
+
