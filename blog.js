bf19fda7f940c24ea895b151d726562da450ff19
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-26 16:12:42 +0900

    login page initial

--- blog.js
+++ blog.js
@@ -7,5 +7,12 @@ menuIcon.addEventListener('click', ()=>{
 //menu icon grabbing and event listener adding
 let profileIcon  = document.querySelector('.js-profile-icon');
 profileIcon.addEventListener('click', ()=>{
-  alert('login');
+  //login page creator html
+  let loginPage = `
+    <div class = 'styles-login-page'>
+      <button>Login</button>
+      <button>Sign Up</button>
+    </div>
+  `;
+  document.querySelector('.login-page-displayer').innerHTML = loginPage;
 });
