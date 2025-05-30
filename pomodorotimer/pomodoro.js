05bb1fafe8dd28968b565d4d93c8def0efbff078
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-30 23:55:27 +0900

    time updating function created

--- pomodorotimer/pomodoro.js
+++ pomodorotimer/pomodoro.js
@@ -7,3 +7,17 @@ const resetElement = document.querySelector('.reset-button');
 let timeInterval;
 //60*25=1500
 let timeLeft = 1500;
+
+function updateTimer(){
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+  let formatedTime = `${
+    minutes.toString().padStart(2, '0')
+  }:
+  ${
+    seconds.toString().padStart(2, '0')
+  }
+  `;
+
+  count.innerHTML = formatedTime;
+}
