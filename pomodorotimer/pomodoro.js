e210cc5f1b9bab12a915b1e8f515e7b42e2d5382
muzaffarsjm <smuzaffarov.1996@gmail.com>
2025-05-30 23:46:39 +0900

    elements grabbed

--- pomodorotimer/pomodoro.js
+++ pomodorotimer/pomodoro.js
@@ -1,1 +1,7 @@
-console.log(56568);
+const count = document.querySelector('.time-displayer');
+
+const startElement = document.querySelector('.start-button');
+const stopElement = document.querySelector('.stop-button');
+const resetElement = document.querySelector('.reset-button');
+
+console.log(count)
